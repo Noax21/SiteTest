f94cf03fcc96ae169fba31be0458a86d289feb31
Noax21 <norfeyz21@gmail.com>
2024-07-12 03:22:06 +0200

    s

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function vérifierPythagore() {
+    const hypoténuse = document.getElementById('hypoténuse').value;
+    const prompt = `Est-ce que ${hypoténuse} est la valeur correcte de l'hypoténuse?`;
+    alert(prompt);
+}
