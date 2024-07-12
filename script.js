07b72a7150c6cb4d4c7e9737ef6a7dbeeacbc0e3
Noax21 <norfeyz21@gmail.com>
2024-07-12 03:21:54 +0200

    s

--- script.js
+++
@@ -1,6 +0,0 @@
-
-function vérifierPythagore() {
-    const hypoténuse = document.getElementById('hypoténuse').value;
-    const prompt = `Est-ce que ${hypoténuse} est la valeur correcte de l'hypoténuse?`;
-    alert(prompt);
-}
